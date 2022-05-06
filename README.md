7b3252b988953817d17dabd37765e61a208b57d6
Gabriel G. Freitas <67321807+gabrielgoncalfreitas@users.noreply.github.com>
2022-05-06 13:26:08 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center">ATN Caldeiraria</h1>
+
+<p align="center">Landing page sendo desenvolvida por Gabriel Freitas</p>
+<br>
+<h4 align="center">Dados</h4>
+<p align="center">
+  GitHub: github.com/gabrielgoncalfreitas <br>
+  WhatsApp: (19) 9 9895-3914 <br>
+  E-mail: gabriel.goncal.freitas@gmail.com <br>
+  Facebook: facebook.com/gabriel.goncal.freitas <br>
+</p>
---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('body').hide();
+    var audio = new Audio("./assets/sound/fade.mp3");
+    audio.play();
+    $('body').fadeIn(1000);
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    navbarShrink();
+
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
